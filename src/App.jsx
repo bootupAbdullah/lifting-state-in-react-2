1e9f04a5ae9707798bb6c16baf8129af0ec94a62
Abdulla Durrani <Abdullahkd@gmail.com>
2024-12-05 22:37:28 -0500

    lecture complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import TodoList from './components/TodoList.jsx';
+
 
 
 const App = () => {
+  const [todos, setTodos] = useState([])
 
   const addTodo = (newTodo) => {
-    console.log(newTodo);
+    console.log([...todos, newTodo]);
+    setTodos([...todos, newTodo]);
+    
+
   };
 
   return (
     <>
       <h1>To-do App</h1>
+      <NewTodo addTodo={addTodo} />
+      <TodoList todos={todos} />
     </>
   );
 };
--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 const NewTodo = (props) => {
     const handleSubmit = (event) => {
       event.preventDefault();
-      console.log("Creating a new to-do...");
+      props.addTodo(newTodo)
+      setNewTodo('')
     };
+
+    const  [newTodo, setNewTodo] = useState('');
+
+    const handleChange = (event) => {
+        setNewTodo(event.target.value)
+    }
   
     return (
       <form onSubmit={handleSubmit}>
         <label htmlFor="todoInput">Todo: </label>
-        <input id="todoInput" type="text" name="todo" />
+        <input 
+        id="todoInput" 
+        type="text" 
+        name="todo" 
+        value={newTodo}
+        onChange={handleChange}
+        />
+
         <button type="submit">Create to-do</button>
       </form>
     );
